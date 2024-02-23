5b1b1269da6f929e4120c6e18969f47fc1b1a595
Mehdi SILINI <mehdi.silini@chanel.com>
2024-02-23 16:07:57 +0100

    update HTTP client to be generic

--- WeaMap/Services/Networking/HTTPClient.swift
+++ WeaMap/Services/Networking/HTTPClient.swift
@@ -15,19 +15,53 @@ enum NetworkError: Error {
     case invalidURL
 }
 
+enum HttpMethod {
+    case get([URLQueryItem])
+    
+    var name: String {
+        switch self {
+        case .get:
+            return "GET"
+        }
+    }
+}
+
+struct Resource<T: Codable> {
+    let url: URL
+    let headers: [String: String] = [:]
+    var method: HttpMethod = .get([])
+}
+
 class HTTPClient {
-    func getWeather(url: URL) async throws -> WeatherDTO {
-        let (data, response) = try await URLSession.shared.data(from: url)
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T {
+        var request = URLRequest(url: resource.url)
+        request.allHTTPHeaderFields = resource.headers
+        request.httpMethod = resource.method.name
         
+        switch resource.method {
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: true)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.badUrl
+            }
+            request.url = url
+        }
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-Type": "application/json"]
+        
+        let session = URLSession(configuration: configuration)
+        let (data, response) = try await session.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else {
+              (200...299).contains(httpResponse.statusCode) else {
             throw NetworkError.invalidResponse
         }
         
-        guard let weatherDTO = try? JSONDecoder().decode(WeatherDTO.self, from: data) else {
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
             throw NetworkError.decodingError
         }
         
-        return weatherDTO
+        return result
     }
 }
